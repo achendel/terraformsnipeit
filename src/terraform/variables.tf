3d1a9a89fb54f251716263c88b322df3f9c459f3
achendel <113882803+achendel@users.noreply.github.com>
2023-11-14 14:50:48 +0530

    Update variables.tf

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -5,12 +5,7 @@ terraform {
       version = "3.79.0"
     }
   }
-   backend "azurerm" {
-    resource_group_name  = "friday-demo-rg"
-    storage_account_name = "sttfstatemgt01"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
+  
 }
 
 provider "azurerm" {
